e124f5771dcde36176529421688b31fed65d7881
Andy Bradford <andy.brad@gmail.com>
2018-07-20 22:55:23 +0100

    Corrected casing on user routes

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '../../../node_modules/@angular/router';
 
-import { UserRoutes } from './user.routes';
+import { userRoutes } from './user.routes';
 import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forChild(UserRoutes)
+    RouterModule.forChild(userRoutes)
   ],
   declarations: [ProfileComponent]
 })
