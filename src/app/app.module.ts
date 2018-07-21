f8666da4d1184ead543530370edeb88b01248617
Andy Bradford <andy.brad@gmail.com>
2018-07-21 13:12:57 +0100

    Added ngx bootstrap drop down

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule }    from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+// JWT
 import { JwtInterceptor, ErrorInterceptor } from './_helpers';
 
-import { AppComponent } from './app.component';
+// NG Bootstrap
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
+// Toaster
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+
+import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { LoginComponent } from './login';
 import { UiModule } from './ui/ui.module';
 import { DashboardModule } from './dashboard/dashboard.module';
-import { ToastrModule } from 'ngx-toastr';
-import { ModalModule } from 'ngx-bootstrap/modal';
 
 @NgModule({
   declarations: [
@@ -34,6 +39,7 @@ import { ModalModule } from 'ngx-bootstrap/modal';
       preventDuplicates: true,
     }),
     ModalModule.forRoot(),
+    BsDropdownModule.forRoot(),
 
     UiModule,
     DashboardModule
